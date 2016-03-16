284408676b6b7de2565398181a5858aba67b59fe
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2016-03-16 20:36:16 +0100

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315100301) do
+ActiveRecord::Schema.define(version: 20160316192032) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "content",    limit: 255
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           limit: 255
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 20160315100301) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "messages", "users"
 end
