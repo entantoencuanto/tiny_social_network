c1fae2225777b914a8ac5d892fa607800bb4b3a8
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2016-03-16 20:41:22 +0100

    Add association with messages in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates :user_name, presence:   true,
                         uniqueness: true
   has_secure_password
+
+  has_many :messages, dependent: :destroy
 end
