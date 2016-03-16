09e004ba81fc8b3c1f2fba4ea579a7c835b907c3
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2016-03-16 20:47:30 +0100

    Add validations to messages
    * Content must be present
    * Content maximum length is 160 characters
    * User must be present

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
+  validates :content, presence: true,
+                      length:   { maximum: 160 }
+  validates :user,    presence: true
+
   belongs_to :user
 end
