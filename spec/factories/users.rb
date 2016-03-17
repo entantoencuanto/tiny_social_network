b17e4118733c4a053b56613e733dea00bceb6102
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2016-03-17 12:02:30 +0100

    Improve users and messages factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,11 @@ FactoryGirl.define do
     email "test@test.com"
     password "1234567890"
     password_confirmation "1234567890"
-    user_name "MyString"
+    user_name { generate :user_name }
   end
+
+  sequence :user_name do |n|
+    "user_name_#{n}"
+  end
+
 end
