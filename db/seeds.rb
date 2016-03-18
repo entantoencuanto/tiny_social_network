407b05cc0af1ee5c17a37e7710ac16a8ed60187d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2016-03-18 11:46:38 +0100

    Add seeds to initialize database in development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development?
+  (1..10).each do |i|
+    user = User.find_by(user_name: "test_#{i}") || User.create(email: "test_#{i}@test.com", user_name: "test_#{i}", password: "12345", password_confirmation: "12345")
+    user.follow(user)
+    user.messages.create(content: "Hello world, I'm ##{i} user!") unless user.messages.present?
+  end
+end
